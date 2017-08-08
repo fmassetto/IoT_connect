7e25708d1a0525a903ebd758637e7918fa3b71cf
Professor Isidro <fmassetto@gmail.com>
2017-08-08 16:28:33 -0300

    First Version

---
+++ IoT_connect.cpp
@@ -0,0 +1,118 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include "IoT_connect.h"
+
+Site::Site(){
+	Serial.begin(115200);
+
+}
+void   Site::definirIP(int oc1, int oc2, int oc3, int oc4){
+	if (modoDebug){
+		Serial.println("Definindo endereco IP");
+		Serial.print(oc1);
+		Serial.print(".");
+		Serial.print(oc2);
+		Serial.print(".");
+		Serial.print(oc3);
+		Serial.print(".");
+		Serial.println(oc4);
+	}
+	IPAddress _rote(oc1, oc2, oc3, 1);
+	IPAddress _plac(oc1, oc2, oc3, oc4);
+	IPAddress _mask(255,255,255,0);
+
+	enderecoPlaca = _plac;
+	roteador      = _rote;
+
+	WiFi.config(enderecoPlaca, roteador, _mask);
+
+}
+
+void   Site::inicializar(){	
+	server = new WiFiServer(80);
+	server->begin();
+	if (modoDebug){
+		Serial.println("Servidor Inicializado!");
+	}
+}
+
+
+void   Site::inserirComando(String botao, String comando){
+	botoes[indexBotoes][0] = botao;
+	botoes[indexBotoes][1] = comando;
+	indexBotoes++;
+	if (modoDebug){
+		Serial.println("Comando Adicionado!");
+	}
+} 
+
+String    Site::receberComando(){
+	String linha = wifiClient.readStringUntil('\n');
+	if (modoDebug){
+		Serial.print("URL Received: ");
+		Serial.println(linha);
+	}
+	wifiClient.flush();
+	return (linha);
+}
+
+int    Site::compararComando(String comando, String nome){
+	return (comando.indexOf(nome) != -1? 1: 0);
+}
+
+void   Site::conectarNaRede(String ssid, String senha){
+	char* ch_ssid;
+	char* ch_senha;
+	ssid.toCharArray(ch_ssid, ssid.length());
+	senha.toCharArray(ch_senha, senha.length());
+	WiFi.begin(ch_ssid, ch_senha);
+    while (WiFi.status() != WL_CONNECTED){
+		delay(500);
+		Serial.print(".");
+	}
+}
+
+void  Site::definirModoDebug(int modo){
+	modoDebug = modo;
+	Serial.println("Definindo Modo debug - LIGADO");
+}
+
+int   Site::temConexao(){
+	//Serial.println("Aguardando Cliente.");
+    wifiClient = server->available();
+    if (!wifiClient){
+    	return 0;
+    }
+    while(!wifiClient.available()){
+		delay(1);
+	}
+	return 1;
+	
+} 
+void   Site::gerarPaginaHTML(){
+	String s="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n";
+	s +="<html>\r\n";
+	s += "<head>\r\n";
+	s +=" <title> .: IoT_Connect :. </title> \r\n";
+	s +=" <style>";
+	s +=" a{background-color:BLUE; color:WHITE; font-size:100px};";
+	s +="";
+	s +="";
+	s +="";
+	s +=" </style>";
+	
+	s += "</head>\r\n";
+	s += "<body>\r\n";
+
+	s += "<h1>Welcome to IoT Connect Library</h1>\r\n";
+	for (int i=0;i<indexBotoes;i++){
+		s+="<a href=\"?"+botoes[i][1]+"\">"+botoes[i][0]+"</a><br/>";
+	}
+	s += "</body>\r\n";
+	s +="</html>";
+	wifiClient.print(s);
+	wifiClient.flush();
+	wifiClient.stop();
+
+}
---
+++ IoT_connect.h
@@ -0,0 +1,47 @@
+#ifndef esp9266wifi_h
+#include <ESP8266WiFi.h>
+#endif
+
+#ifndef wificlient_h
+#include <WiFiClient.h>
+#endif
+
+#ifndef esp8266webserver_h
+#include <ESP8266WebServer.h>
+#endif
+
+typedef String Comando;
+typedef String Pagina;
+
+class Site{
+    private:
+       IPAddress  enderecoPlaca;
+       IPAddress  roteador;
+       String     ssid;
+       String     password;
+       
+       String     botoes[10][2];
+       int        indexBotoes;
+
+       WiFiClient wifiClient;
+       WiFiServer *server;
+
+       String     titulo;
+       String     cabecalho;
+       String     rodape;
+       int        modoDebug=0;
+       String     corFundoPagina;
+
+    public: 
+
+       Site();
+       void      definirIP(int oc1, int oc2, int oc3, int oc4);
+       void      inserirComando(String botao, String comando);  
+       Comando   receberComando();
+       int       compararComando(String comando, String nome);
+       void      inicializar();
+       int       temConexao();
+       void      conectarNaRede(String ssid, String senha); 
+       void      gerarPaginaHTML();
+       void      definirModoDebug(int modo);
+};
